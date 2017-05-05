b63c6bef75fbc545fb1db57fa41200c688c93c6a
Fernando Carletti <contato@fernandocarletti.net>
2017-05-05 12:19:15 -0500

    Fixed aws_ec2 not being exposed to SSHKit.

--- lib/capistrano/aws.rb
+++ lib/capistrano/aws.rb
@@ -2,6 +2,8 @@ require 'capistrano/aws/ec2/ec2'
 require 'capistrano/dsl/aws'
 extend Capistrano::DSL::Aws
 
+SSHKit::Backend::Netssh.send(:include, Capistrano::DSL::Aws)
+
 load File.expand_path('../tasks/ec2.rake', __FILE__)
 
 namespace :load do
